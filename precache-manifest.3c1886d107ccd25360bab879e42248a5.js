471533113e4707c1fa33e7300ffeb7a9e8ab82a3
Rana Emad <rana3emad@gmail.com>
2020-06-02 16:23:51 +0200

    Updates

---
+++ precache-manifest.3c1886d107ccd25360bab879e42248a5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6f9596fc65d866e3e6f1667cdd6521",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "f11b51991e062b52d0f0",
+    "url": "/calendar/static/css/main.16cfe633.chunk.css"
+  },
+  {
+    "revision": "baf186ca37f6b86fefa2",
+    "url": "/calendar/static/js/2.ce2dfe92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calendar/static/js/2.ce2dfe92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f11b51991e062b52d0f0",
+    "url": "/calendar/static/js/main.f7589b32.chunk.js"
+  },
+  {
+    "revision": "5e1efa41e70e9f825195",
+    "url": "/calendar/static/js/runtime-main.05ca6ea8.js"
+  },
+  {
+    "revision": "664aa5d7a9d3f8820616a6e17e768d32",
+    "url": "/calendar/static/media/friday.664aa5d7.svg"
+  },
+  {
+    "revision": "6af1656621e0c44624f50c7167eaf416",
+    "url": "/calendar/static/media/monday.6af16566.svg"
+  },
+  {
+    "revision": "64d0c4b5a244b566361cb650f12f6e2c",
+    "url": "/calendar/static/media/saturday.64d0c4b5.svg"
+  },
+  {
+    "revision": "0256b07845db59084042c687e78e76f3",
+    "url": "/calendar/static/media/sunday.0256b078.svg"
+  },
+  {
+    "revision": "76a23e166e997b60637235f1cb1d686e",
+    "url": "/calendar/static/media/thursday.76a23e16.svg"
+  },
+  {
+    "revision": "c5117fae1f7070d7294b1c1dcba76ac1",
+    "url": "/calendar/static/media/tuesday.c5117fae.svg"
+  },
+  {
+    "revision": "686bfb6b757c5dc8102b7d65fe5f6163",
+    "url": "/calendar/static/media/wednesday.686bfb6b.svg"
+  }
+]);
